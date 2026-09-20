526b25365ca6997914551fb5632fd152217daf21
Brandon Lazala <bralaz4151@students.ecpi.edu>
2026-09-19 21:34:06 -0400

    Add temperature conversion functionality

--- AppMenu.py
+++ AppMenu.py
@@ -2,6 +2,23 @@ from datetime import datetime
 
 menu_options = ["Input Data", "View Current Data", "Generate Report"]
 
+
+def convertData(value):
+    # convertData takes the temperature in Fahrenheit and returns it converted to Celsius
+    return (value - 32) * 5 / 9
+
+
+def getInput():
+    num_entries = int(input("How many entries are you inputting? "))
+    for i in range(num_entries):
+        date = input("Enter a date: ")
+        temp = int(input("Enter the highest temp for the inputted date: "))
+        # convertData takes one argument, the temperature in Fahrenheit, and returns the temperature converted to Celsius
+        converted = convertData(temp)
+        print("The following was saved at " + str(datetime.now()) + " : ")
+        print(date + "," + str(temp) + "," + str(converted))
+
+
 print("bralaz4151 Spreadsheet Automation Menu")
 print("Choose a number from the following options")
 
@@ -14,5 +31,9 @@ choice = input()
 
 if choice.isdigit() and 1 <= int(choice) <= len(menu_options):
     print("You selected " + choice + " at " + str(datetime.now()))
+    if choice == "1":
+        getInput()
+    else:
+        print("Error: The chosen functionality is not implemented yet")
 else:
     print("Error: Invalid choice selected.")
